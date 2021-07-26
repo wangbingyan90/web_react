cab8432d48ee2480299eff22fcbf42ac8b87cf66
wby <1418885488@qq.com>
2021-07-27 00:20:54 +0800

    p10

--- src/components/sandbox/SideMent.js
+++ src/components/sandbox/SideMent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout, Menu} from 'antd';
+import './index.css'
 import {
     UserOutlined,
     VideoCameraOutlined,
@@ -7,11 +8,21 @@ import {
   } from '@ant-design/icons';
 
 const { Sider } = Layout;
+const { SubMenu } = Menu;
+
+const menuList = [
+    {
+        key:"",
+        title:"",
+        icon:""
+    }
+]
+
 export default function SideMent() {
     return (
-        <Sider trigger={null} collapsible>
-            <div className="logo" />
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+        <Sider trigger={null} collapsible collapsed={false}>
+            <div className="logo">loglalal</div>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['3']}>
                 <Menu.Item key="1" icon={<UserOutlined />}>
                     nav 1
             </Menu.Item>
@@ -21,6 +32,12 @@ export default function SideMent() {
                 <Menu.Item key="3" icon={<UploadOutlined />}>
                     nav 3
             </Menu.Item>
+            <SubMenu key="sub4" icon={<UploadOutlined/>} title="asasd">
+                <Menu.Item key="9">9</Menu.Item>
+                <Menu.Item key="10">10</Menu.Item>
+                <Menu.Item key="11">11</Menu.Item>
+                <Menu.Item key="12">12</Menu.Item>
+            </SubMenu>
             </Menu>
         </Sider>
     )
