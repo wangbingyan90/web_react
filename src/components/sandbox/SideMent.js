2a15eb22c3700764be2c26d558b45241b3e99c0e
wby <1418885488@qq.com>
2021-07-29 23:13:34 +0800

    p15

--- src/components/sandbox/SideMent.js
+++ src/components/sandbox/SideMent.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { Layout, Menu} from 'antd';
 import './index.css'
 import {withRouter} from 'react-router-dom'
+import axios from 'axios';
 import {
     UserOutlined,
     VideoCameraOutlined,
@@ -11,54 +12,38 @@ import {
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
-const menuList = [
-    {
-        key:"/home",
-        title:"首页",
-        icon: <UserOutlined />
-    },
-    {
-        key:"/user-manage",
-        title:"用户管理",
-        icon: <UploadOutlined />,
-        children:[{
-            key:"/user-manage/list",
-            title:"用户列表",
-            icon: <VideoCameraOutlined />
-        }]
-    },
-    {
-        key:"/right-manage",
-        title:"权限管理",
-        icon: <UserOutlined />,
-        children:[{
-            key:"/right-manage/role/list",
-            title:"角色列表",
-            icon: <UserOutlined />  
-        },
-        {
-            key:"/right-manage/right/list",
-            title:"权限列表",
-            icon: <UserOutlined />
-        }
-    ]
-    }
-]
+const iconList = {
+    "/home": <UserOutlined />,
+    "/user-manage/list": <VideoCameraOutlined />,
+    "/right-manage/role/list": <UploadOutlined />,
+    "/right-manage/right/list": <VideoCameraOutlined />,
+    "/user-manage": <UploadOutlined />,
+    "/right-manage": <UserOutlined />
+}
 
+ function SideMent(props) {
+    const [menu,setMenu] = useState([])
 
- function SideMent(props  ) {
+    useEffect(()=>{
+        axios.get("http://localhost:8000/posts").then(
+                res=>{
+                    console.log(res.data)
+                    setMenu(res.data)
+                }
+        )
+    },[])
     
     const renderMenu = (menuList) =>{
         return menuList.map(item=>{
             if(item.children){
                 return <SubMenu key={item.key} 
-                                icon={item.icon}
+                                icon={iconList[item.key]}
                                 title={item.title}>
                                 {renderMenu(item.children)}
                                 </SubMenu>
                 
             }
-            return <Menu.Item key={item.key} icon={item.icon} onClick={()=>{
+            return <Menu.Item key={item.key} icon={iconList[item.key]} onClick={()=>{
                 props.history.push(item.key)
             }}>{item.title}</Menu.Item>
         }
@@ -70,7 +55,7 @@ const menuList = [
         <Sider trigger={null} collapsible collapsed={false}>
             <div className="logo">loglalal</div>
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['3']}>
-            {renderMenu(menuList)}
+            {renderMenu(menu)}
             </Menu>
         </Sider>
     )
